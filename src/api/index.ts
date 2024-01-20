d7959f648cefbeb403bf92ed8f82887e6ffce774
Progress Aienobe <paienobe@gmail.com>
2024-01-20 13:51:31 +0100

    update-task

--- src/api/index.ts
+++ src/api/index.ts
@@ -28,3 +28,27 @@ export const deleteTask = async (taskId: string, accessToken: string) => {
     throw new Error("Failed to delete task");
   }
 };
+
+export const updateTask = async (
+  taskId: string,
+  isCompleted: boolean,
+  accessToken: string
+) => {
+  try {
+    const request = await goTodoInstance.put(
+      `/update`,
+      {
+        id: taskId,
+        isCompleted,
+      },
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }
+    );
+    const response = request.data;
+    return response;
+  } catch (error) {
+    toast.error("Failed to update task!");
+    throw new Error("Failed to update task");
+  }
+};
--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -2,31 +2,50 @@ import "./TodoItem.css";
 import { TodoItemProps } from "./types";
 import cross from "../../assets/icon-cross.svg";
 import check from "../../assets/icon-check.svg";
-import { deleteTask } from "../../api";
+import { deleteTask, updateTask } from "../../api";
 import { useGlobalContext } from "../../context/globalContext/GlobalContext";
 import { toast } from "react-toastify";
+import { Task } from "../../types";
 
 const TodoItem = ({ task }: TodoItemProps) => {
   const { accessToken, tasks, setTasks } = useGlobalContext();
+
+  const handleDelete = () => {
+    deleteTask(task.id, accessToken).then(() => {
+      const updatedTasks = tasks.filter((item) => {
+        return item.id != task.id;
+      });
+      setTasks(updatedTasks);
+      toast.success("Task deleted!");
+    });
+  };
+
+  const handleUpdate = () => {
+    updateTask(task.id, !task.isCompleted, accessToken).then((result: Task) => {
+      const updatedTasks = tasks.map((item) => {
+        if (item.id == task.id) {
+          return result;
+        } else return item;
+      });
+      setTasks(updatedTasks);
+      toast.success("Task updated!");
+    });
+  };
+
   return (
     <div className="todo_item">
       <div>
-        <div>{task.isCompleted && <img src={check} alt="" />}</div>
-        <p>{task.name}</p>
+        <div
+          onClick={handleUpdate}
+          className={task.isCompleted ? "completed_task" : ""}
+        >
+          {task.isCompleted && <img src={check} alt="" />}
+        </div>
+        <p className={task.isCompleted ? "completed_task_name" : ""}>
+          {task.name}
+        </p>
       </div>
-      <img
-        src={cross}
-        alt=""
-        onClick={() => {
-          deleteTask(task.id, accessToken).then(() => {
-            const updatedTasks = tasks.filter((item) => {
-              return item.id != task.id;
-            });
-            setTasks(updatedTasks);
-            toast.success("Task deleted!");
-          });
-        }}
-      />
+      <img src={cross} alt="" onClick={handleDelete} />
     </div>
   );
 };
